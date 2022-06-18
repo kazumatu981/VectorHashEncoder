643c409a9baa18247ce6e91d61e856b9e10a1f8a
kazumatu981 <kazumatu981@gmail.com>
2022-06-18 09:58:02 +0900

    TimeSpaceHashEncoder Implemented.

---
+++ VectorHashEncoder/TImeSpaceHashEncoder.cs
@@ -0,0 +1,29 @@
+namespace VectorHashEncoder;
+
+public class TimeSpaceHashEncoder : PrimitiveHashEncoder
+{
+    private static readonly (double, double)[] DefaultBoundary =
+        new[]
+        {
+            (-180.0, 180.0),
+            (-90.0, 90.0) ,
+            (0, 24 * 60 * 60 * 1000)
+        };
+    public TimeSpaceHashEncoder() : base(DefaultBoundary, EncoderUtils.Base32Charactors)
+    {
+        // no-constructor action except base constructor action.
+    }
+
+    public string EncodeToString(GeoLocation geoLocation, DateTime time, int levels)
+    {
+        var timeValue = (((time.Hour * 60) + time.Minute) * 60 + time.Second) * 1000 + time.Millisecond;
+
+        return EncodeToString(new[]
+        {
+            geoLocation.Longitude,
+            geoLocation.Latitude,
+            timeValue
+        }, levels);
+    }
+
+}
